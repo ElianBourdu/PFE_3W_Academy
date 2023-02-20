710167e674ef22b23ae00fdea8cc58cb62a7e316
elian <elian.bourdu@gmail.com>
2023-02-20 14:20:40 +0000

    clear updateUser.jsx file

--- front/src/components/users/UpdateUser.jsx
+++ front/src/components/users/UpdateUser.jsx
@@ -2,12 +2,10 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "../../tools/utils.js";
 import { useState, useEffect, Fragment } from "react";
-// import moment from 'moment';
 
 const UpdateUser = () => {
     const { id } = useParams();
-    const [user, setUser] = useState({ last_name: '', first_name: '', email: '' });
-
+    const [user, setUser] = useState();
 
     useEffect(() => {
         axios.post(`${BASE_URL}/readUser`, { id })
@@ -23,7 +21,6 @@ const UpdateUser = () => {
 
     const submit = (e) => {
         e.preventDefault();
-        console.log(user);
         axios.post(`${BASE_URL}/updateUser`, { ...user })
             .then(res => console.log(res))
             .catch(err => console.log(err));
@@ -33,8 +30,6 @@ const UpdateUser = () => {
         const date = new Date(data);
         return date.toLocaleDateString("fr-CA");
     };
-    // , { year: 'numeric', month: '2-digit', day: '2-digit' }
-
 
     return (
         <Fragment>
@@ -57,12 +52,6 @@ const UpdateUser = () => {
                         <label htmlFor="birth_date">birth_date</label>
                         <input type='date' name='birth_date' onChange={handleChange} value={reformeDate(user.birth_date)} />
                     </div>
-                    {/*
-                    <select onChange={null} value={null}>
-                        <option value='6'>Admin</option>
-                        <option value='5'>user</option>
-                    </select>
-                    */}
                     <input type='submit' />
                 </form>
             )}
